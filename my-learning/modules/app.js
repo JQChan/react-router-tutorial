e0246fd6b3884a11fcac5d25ebb383cba82159a0
Chenjq <chenjq@lt51.cn>
2017-08-15 15:37:54 +0800

    active Link with activeStyle or activeClassName

--- my-learning/modules/app.js
+++ my-learning/modules/app.js
@@ -8,8 +8,8 @@ export default React.createClass({
       <div>
           <h1>Hello, React Router!</h1>
           <ul role="nav">
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/repos">Repos</Link></li>
+            <li><Link to="/about" activeStyle={{ color: 'red' }} activeClassName="active">About</Link></li>
+            <li><Link to="/repos" activeStyle={{ color: 'red' }} activeClassName="active">Repos</Link></li>
           </ul>
           
           {this.props.children}
