98e234319c963a2c82a55109b65078da2c7dd54f
Chenjq <chenjq@lt51.cn>
2017-08-15 16:22:44 +0800

    更深层的嵌套

--- my-learning/index.js
+++ my-learning/index.js
@@ -3,13 +3,16 @@ import { render } from 'react-dom';
 import App from './modules/App';
 import About from './modules/About';
 import Repos from './modules/Repos';
+import Repo from './modules/Repo';
 import { Router, Route, hashHistory  } from 'react-router';
 
 render(
   (<Router history={hashHistory}>
        <Route path="/" component={App}>
           <Route path="/about" component={About} />
-          <Route path="/repos" component={Repos} />
+          <Route path="/repos" component={Repos}>
+             <Route path="/repos/:username/:repoName" component={Repo} />
+          </Route>
        </Route>
     </Router>
   ) , document.getElementById('app')
