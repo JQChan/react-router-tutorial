6be63a9963ec0e30889757554bf1bece55de3bfa
Chenjq <chenjq@lt51.cn>
2017-08-15 16:46:47 +0800

    use IndexLink or use onlyActiveOnIndex to active index link

--- my-learning/modules/app.js
+++ my-learning/modules/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
+import NavLink from './NavLink';
 
 export default React.createClass({
   render () {
@@ -10,6 +11,9 @@ export default React.createClass({
           <ul role="nav">
             <li><Link to="/about" activeStyle={{ color: 'red' }} activeClassName="active">About</Link></li>
             <li><Link to="/repos" activeStyle={{ color: 'red' }} activeClassName="active">Repos</Link></li>
+            <li><NavLink to="/" >Home</NavLink></li>
+            <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
+            <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
           </ul>
           
           {this.props.children}
